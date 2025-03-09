5118dfe82bef22636a5357b668f3e715c471c65d
PaxtonTerryDev <paxtonterrydev@gmail.com>
2025-03-09 14:25:03 -0600

    update commit

--- Graphical/Resolution.cs
+++ Graphical/Resolution.cs
@@ -5,6 +5,8 @@ namespace Settings.Graphical;
 
 public partial class Resolution: Node
 {
+    public Vector2I SelectedResolution { get; set; } = new Vector2I(1920, 1080);
+    
     public static readonly Dictionary<string, Vector2I> Resolutions = new Dictionary<string, Vector2I>
     {
         { "2560x1440", new Vector2I(2560, 1440) },
@@ -21,4 +23,24 @@ public partial class Resolution: Node
     };
 
     public Array<string> DisplayOptions => (Array<string>)Resolutions.Keys;
+
+    private bool IsValidResolution(string resolutionString)
+    {
+        return Resolutions.ContainsKey(resolutionString);
+    }
+
+    public bool ChangeResolution(string resolution)
+    {
+        if (IsValidResolution(resolution))
+        {
+            SelectedResolution = new Vector2I(SelectedResolution.X, SelectedResolution.Y);
+        }
+
+        return false;
+    }
+
+    private void SetSelectedResolution(Vector2I resolution)
+    {
+        
+    }
 }
